535c7d7b9b93fe58cf8a59b3913c6f5cc807a4c8
Gbolahan Akande <geakande@gmail.com>
2025-04-02 18:58:00 +0100

    feat(types): add CHAIN_BRAND_40 branded type

--- frontend/src/types/stacks-chain.types.ts
+++ frontend/src/types/stacks-chain.types.ts
@@ -137,3 +137,6 @@ export type CHAIN_BRAND_38 = string & { readonly _brand: 'CHAIN_BRAND_38' };
 
 /** CHAIN_BRAND type alias 39 */
 export type CHAIN_BRAND_39 = string & { readonly _brand: 'CHAIN_BRAND_39' };
+
+/** CHAIN_BRAND type alias 40 */
+export type CHAIN_BRAND_40 = string & { readonly _brand: 'CHAIN_BRAND_40' };
