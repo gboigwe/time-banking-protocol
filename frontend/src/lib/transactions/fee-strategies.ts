8aff1595264c5d6848fdafc4904268e864ac8724
Gbolahan Akande <geakande@gmail.com>
2025-04-02 10:07:00 +0100

    feat(transactions): add MinFeeStrategy class

--- frontend/src/lib/transactions/fee-strategies.ts
+++ frontend/src/lib/transactions/fee-strategies.ts
@@ -4,3 +4,8 @@
 export interface FeeStrategy {
   estimateFee(): bigint;
 }
+
+/** Minimum fee strategy */
+export class MinFeeStrategy implements FeeStrategy {
+  estimateFee(): bigint { return 180n; }
+}
