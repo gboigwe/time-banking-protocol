5dbede5349e2ef4877359666371b19d2a04f1158
Gbolahan Akande <geakande@gmail.com>
2025-04-02 19:58:00 +0100

    feat(connect): add TRANSFER_LIMIT constant 9

--- frontend/src/lib/connect/stx-transfer-flow.ts
+++ frontend/src/lib/connect/stx-transfer-flow.ts
@@ -53,3 +53,6 @@ export const TRANSFER_LIMIT_7 = 49;
 
 /** TRANSFER_LIMIT constant 8 */
 export const TRANSFER_LIMIT_8 = 56;
+
+/** TRANSFER_LIMIT constant 9 */
+export const TRANSFER_LIMIT_9 = 63;
