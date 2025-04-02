1b36684ca5cd40f95aac027343d773f109744d24
Gbolahan Akande <geakande@gmail.com>
2025-04-02 16:10:00 +0100

    feat(types): add TB_BRAND_84 branded type

--- frontend/src/types/time-banking.types.ts
+++ frontend/src/types/time-banking.types.ts
@@ -307,3 +307,6 @@ export type TB_BRAND_82 = string & { readonly _brand: 'TB_BRAND_82' };
 
 /** TB_BRAND type alias 83 */
 export type TB_BRAND_83 = string & { readonly _brand: 'TB_BRAND_83' };
+
+/** TB_BRAND type alias 84 */
+export type TB_BRAND_84 = string & { readonly _brand: 'TB_BRAND_84' };
