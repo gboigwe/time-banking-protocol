c8940839bb4cc4967ae986669d3905d50e970daf
Gbolahan Akande <geakande@gmail.com>
2025-04-02 21:43:00 +0100

    feat(connect): add TRANSFER_LIMIT constant 44

--- frontend/src/lib/connect/stx-transfer-flow.ts
+++ frontend/src/lib/connect/stx-transfer-flow.ts
@@ -158,3 +158,6 @@ export const TRANSFER_LIMIT_42 = 294;
 
 /** TRANSFER_LIMIT constant 43 */
 export const TRANSFER_LIMIT_43 = 301;
+
+/** TRANSFER_LIMIT constant 44 */
+export const TRANSFER_LIMIT_44 = 308;
